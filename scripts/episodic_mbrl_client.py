a9965742c85a4f7d835a2973ae2b4856e5a74ed0
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2018-01-16 17:42:07 -0400

    added default mc_pilco learning function

--- scripts/episodic_mbrl_client.py
+++ scripts/episodic_mbrl_client.py
@@ -10,13 +10,16 @@ import numpy as np
 import os
 import rospy
 import threading
-import traceback
 import yaml
 
+from collections import OrderedDict
+from functools import partial
 from Queue import Queue, Empty
 from ros_plant import ROSPlant
 
-from kusanagi.base import apply_controller
+from kusanagi.base import (apply_controller, train_dynamics,
+                           preprocess_angles, ExperienceDataset)
+from kusanagi.ghost.algorithms import mc_pilco
 from kusanagi.ghost.control import RandPolicy
 
 
@@ -57,9 +60,69 @@ def parse_config(config_path):
     return config
 
 
-def dummy_target(task_name, task_spec, experience, task_queue):
+def mc_pilco_polopt(task_name, task_spec, experience, task_queue):
+    '''
+    executes one iteration of mc_pilco (model updating and policy optimization)
+    '''
+    # get task specific variables
+    n_samples = task_spec['n_samples']
+    dyn = task_spec['transition_model']
+    exp = task_spec.get('experience_data', ExperienceDataset())
+    pol = task_spec['policy']
+    plant_params = task_spec['plant']
+    immediate_cost = task_spec['cost']['graph']
+
+    # append new experrience to dataset
+    task_state[task_name] = 'update_dyn'
+    states, actions, costs, infos = experience
+    ts = [info.get('t', None) for info in infos]
+    exp.append_episode(states, actions, costs, infos,
+                       pol.get_params(symbolic=False), ts)
+    task_spec['experience_data'] = exp
+
+    # train dynamics model. TODO block if training multiple tasks with
+    # the same model
+    train_dynamics(dyn, exp, pol.angle_dims,
+                   wrap_angles=task_spec['wrap_angles'])
+
+    # init policy optimizer if needed
+    optimizer = task_spec['optimizer']
+    if optimizer.loss_fn is None:
+        task_state[task_name] = 'compile_polopt'
+        import theano.tensor as tt
+        ex_in = OrderedDict([(k, v) for k, v in immediate_cost.keywords.items()
+                             if type(v) is tt.TensorVariable
+                             and len(v.get_parents()) == 0])
+        task_spec['extra_in'] = ex_in
+        loss, inps, updts = mc_pilco.get_loss(
+            pol, dyn, immediate_cost, n_samples=n_samples, **ex_in)
+        inps += ex_in.values()
+        optimizer.set_objective(
+            loss, pol.get_params(symbolic=True), inps, updts)
+
+    # train policy # TODO block if learning a multitask policy
+    task_state[task_name] = 'update_polopt'
+    # build inputs to optimizer
+    p0 = plant_params['state0_dist']
+    H = int(np.ceil(task_spec['horizon_secs']/plant_params['dt']))
+    gamma = task_spec['discount']
+    polopt_args = [p0.mean, p0.cov, H, gamma]
+    extra_in = task_spec.get('extra_in', OrderedDict)
+    if len(extra_in) > 0:
+        polopt_args += [task_spec['cost']['params'][k] for k in extra_in]
+    # update dyn and pol (resampling)
+    if hasattr(dyn, 'update'):
+        dyn.update(n_samples)
+    if hasattr(pol, 'update'):
+        pol.update(n_samples)
+    # call minimize
+    optimizer.minimize(*polopt_args,
+                       return_best=task_spec['return_best'])
+
+    # put task in the queue for execution
+    task_state[task_name] = 'ready'
     task_queue.put((task_name, task_spec))
-    print 'called dummy learner'
+
     return
 
 
@@ -94,8 +157,12 @@ if __name__ == '__main__':
 
     # populate task queue
     for task_name in config['tasks']:
-        tasks.put((task_name, config['tasks'][task_name]))
+        spec = config['tasks'][task_name]
+        policy = spec['policy']
         task_state[task_name] = 'init'
+        # trigger policy init (for kusanagi only)
+        policy.evaluate(np.zeros(policy.D))
+        tasks.put((task_name, spec))
 
     # while tasks are not done
     while not all([st == 'done' for st in task_state]):
@@ -121,19 +188,23 @@ if __name__ == '__main__':
             # collect random experience
             policy = RandPolicy(maxU=spec['policy'].maxU,
                                 random_walk=spec.get('random_walk', False))
-            polopt_fn = dummy_target
+            polopt_fn = mc_pilco_polopt
         else:
             # TODO load policy parameters from disk
             policy = spec['policy']
-            polopt_fn = spec.get('polopt__fn',
-                                 config.get('default_polopt__fn',
-                                            dummy_target))
+            polopt_fn = spec.get('polopt_fn',
+                                 config.get('default_polopt_fn',
+                                            mc_pilco_polopt))
 
         # set task horizon
         H = int(np.ceil(spec['horizon_secs']/env.dt))
 
         # execute tasks and collect experience data
-        experience = apply_controller(env, policy, H)
+        preprocess = None
+        if hasattr(policy, 'angle_dims'):
+            preprocess = partial(
+                preprocess_angles, angle_dims=policy.angle_dims)
+        experience = apply_controller(env, policy, H, preprocess)
 
         # launch learning in a separate thread
         new_thread = threading.Thread(name=name, target=polopt_fn,
