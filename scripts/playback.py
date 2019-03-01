2f157a4cd0f78324a7fa90de371d67870999e171
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2019-03-01 15:51:45 -0500

    updated playback script

--- scripts/playback.py
+++ scripts/playback.py
@@ -26,18 +26,30 @@ if __name__ == '__main__':
     np.set_printoptions(linewidth=200, precision=3)
     rospy.init_node('kusanagi_ros', disable_signals=True)
 
-    parser = argparse.ArgumentParser(
-        'rosrun robot_learning task_client.py')
+    parser = argparse.ArgumentParser('rosrun robot_learning task_client.py')
     parser.add_argument(
-        'results_path', metavar='DIR',
+        'results_path',
+        metavar='DIR',
         help='The location of the results .dill and zip files',
         type=str)
     parser.add_argument(
-        '-n', '--n_evals', metavar='N', default=1,
+        '-n',
+        '--n_evals',
+        metavar='N',
+        default=1,
         help='Run each policy N times per episode',
         type=int)
     parser.add_argument(
-        '-w', '--cost_width', metavar='w',
+        '-i',
+        '--iter',
+        metavar='I',
+        default=0,
+        help='Start from policy obtained at iteration i',
+        type=int)
+    parser.add_argument(
+        '-w',
+        '--cost_width',
+        metavar='w',
         help='cost_width for distance based cost',
         type=float)
 
@@ -69,13 +81,14 @@ if __name__ == '__main__':
 
     # init environment with task params
     plant_params = config['plant']
+    print 'COST WIDTH', config['cost']['graph'].func.keywords['cw']
     if args.cost_width is not None:
         # replace cost function with new one
         cost_params = config['cost']['params']
         config['cost']['graph'].func.keywords['cw'] = args.cost_width
         loss = config['cost']['graph']
-        plant_params['loss_func'] = build_loss_func(
-            loss, **config['cost']['params'])
+        plant_params['loss_func'] = build_loss_func(loss,
+                                                    **config['cost']['params'])
 
     env = ROSPlant(**plant_params)
 
@@ -84,10 +97,22 @@ if __name__ == '__main__':
     dyn = config['transition_model']
     pol = config['policy']
 
-    H = int(np.ceil(config['horizon_secs']/config['plant']['dt']))
+    H = int(np.ceil(config['horizon_secs'] / config['plant']['dt']))
     config['min_steps'] = H
-    results = evaluate_policy(env, pol, exp, config, n_tests=args.n_evals)
+    #episodes = [i+5 for i in [0, 4, 9, 12, 26, 38, 39]]
+    #episodes = [i+5 for i in [ 0 , 3 , 9, 15, 29, 36, 37]]
+    #episodes = [i+5 for i in [ 0,  4,  9, 18, 26, 39, 46]]
+    #episodes = [i+5 for i in [0,  4, 8, 18, 19, 37, 43]]
+    #episodes = [i+5 for i in [ 0,  4,  8, 19, 24, 33, 49]]
+    #episodes = [i+5 for i in [ 0,  2,  9, 19, 29, 36, 43]]
+    #episodes = [i+5 for i in [ 0,  3,  6, 19, 29, 38, 44]]
+    #episodes = [i+5 for i in [ 0,  4,  9, 17, 26, 38, 39]]
+    episodes = range(args.iter, exp.n_episodes())
+    print episodes
+
+    results = evaluate_policy(
+        env, pol, exp, config, filter_episodes=episodes, n_tests=args.n_evals)
     with open(
-        os.path.join(
-            args.results_path, 'results_%d.dill') % (args.n_evals), 'wb') as f:
+            os.path.join(args.results_path, 'results_%d.dill') %
+        (args.n_evals), 'wb') as f:
         dill.dump(results, f)
