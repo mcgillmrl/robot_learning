ebeda900dc238b19f2b776bbcf72e038f5dcaa3c
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2019-03-01 15:52:33 -0500

    updated the tripod gait

--- scripts/learning_walking.py
+++ scripts/learning_walking.py
@@ -6,8 +6,8 @@ from robot_learning.srv import T2VInfo
 import numpy as np
 
 
-def tripod_gait(state, cmd, dt, period=3.0, offset=4*np.pi/12,
-                sweep_angle=np.pi/2, duty_factor=0.75):
+def tripod_gait(state, cmd, dt, period=1.5, offset=0.785,
+                sweep_angle=1.0, duty_factor=0.685, standing=False):
     leg_angles = cmd[:6]
     slow_legs = (np.abs(
                     (leg_angles - offset + np.pi) % (2*np.pi)
@@ -16,8 +16,8 @@ def tripod_gait(state, cmd, dt, period=3.0, offset=4*np.pi/12,
     slow_legs = np.where(slow_legs)[0]
     fast_legs = np.where(fast_legs)[0]
 
-    slow_speed = -(2*np.pi)/(period*duty_factor)
-    fast_speed = -(2*np.pi)/(period*(1.0-duty_factor))
+    slow_speed = -(2*np.pi)/(2*period*duty_factor)
+    fast_speed = -(2*np.pi)/(2*period*(1.0-duty_factor))
     cmd[6+slow_legs] = slow_speed
     cmd[slow_legs] = (cmd[slow_legs] +
                       dt*slow_speed) % (2*np.pi)
@@ -25,7 +25,13 @@ def tripod_gait(state, cmd, dt, period=3.0, offset=4*np.pi/12,
     cmd[6+fast_legs] = fast_speed
     cmd[fast_legs] = (cmd[fast_legs] +
                       dt*fast_speed) % (2*np.pi)
-    return cmd
+
+    if standing:
+        if 1 in fast_legs and (cmd[1] - offset) % (2*np.pi) < np.pi:
+            standing = False
+        else:
+            cmd[:6:2] = offset
+    return cmd, standing
 
 
 if __name__ == '__main__':
@@ -38,16 +44,19 @@ if __name__ == '__main__':
     command_dims = command_dims_service().value
 
     cmd = np.zeros(command_dims)
-    cmd[:6] = 0.785
-    cmd[1] += np.pi
-    cmd[3] += np.pi
-    cmd[5] += np.pi
+    offset = 0.985
+    cmd[:6] = offset
+
+    state, reward, info, done = env.step(cmd)
+    standing = True
+
+    rospy.sleep(1.0)
+
     prev_t = rospy.get_time()
     while not rospy.is_shutdown():
         t = rospy.get_time()
         dt = (t - prev_t)
         prev_t = t
-
-        cmd = tripod_gait(state, cmd, dt)
-
+        cmd, standing = tripod_gait(
+            state, cmd, dt, offset=offset, standing=standing)
         state, reward, info, done = env.step(cmd)
